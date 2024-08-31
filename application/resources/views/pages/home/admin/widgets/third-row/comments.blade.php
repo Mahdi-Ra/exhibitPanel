d9c2a9456d3f6eab889dae644abf70489e7900ef
Mahdi Rahani <mahdirahani@gmail.com>
2024-08-31 16:43:03 +0330

    first commit

---
+++ application/resources/views/pages/home/admin/widgets/third-row/comments.blade.php
@@ -0,0 +1,9 @@
+<div class="col-lg-4  col-md-12">
+    <div class="card">
+        <div class="card-body text-right">
+            <h5 class="card-title">{{ cleanLang(__('lang.latest_comments')) }}</h5>
+
+
+        </div>
+    </div>
+</div>
